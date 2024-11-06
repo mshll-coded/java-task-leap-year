832e2e7c95b7a41ad38b8e855ab0a2c1bb028647
meshal <meshal7@pdx.edu>
2024-11-06 11:21:46 +0300

    complete task

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a year pls:");
+        int year = scanner.nextInt();
+
+        System.out.println(((year % 4 == 0) || (year % 100 != 0 && year % 400 == 0)) ? "Leap year" : "NOT a leap year");
+
+        scanner.close();
+    }
 }
